10b547261cfc3eb9cb59d1a960a941cc7d005c72
bhawna <bhawnakushwaha727@gmail.com>
2024-09-09 23:13:05 +0530

    changes in server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
-const express=require('express')
-const app=express();
-const cors=require('cors')
-const dotenv=require('dotenv')
-dotenv.config({path:'./server/.env'});
-const testRoute=require('./routes/testRoutes');
-const path = require('path');
-app.use(express.json())
-app.use(cors())
-app.use('/api/v1/test',testRoute)
- const PORT=process.env.PORT
-app.listen(PORT,(req,res)=>{
-    console.log('server is running fine')
-})
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const testRoute = require('./routes/testRoutes');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1/test', testRoute);
+
+// General error handler
+app.use((err, req, res, next) => {
+    console.error("Unhandled error:", err);
+    res.status(500).send({
+        message: "Something went wrong",
+        success: false
+    });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
