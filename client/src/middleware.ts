c1905e837eb00d52692be9d9ebbc31e8e2db4293
Shubham Karmveer <s.karmveer1201@gmail.com>
2025-03-25 03:49:27 +0530

    Update middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,23 +5,23 @@ const publicPaths = ['/login', '/forget-password'];
 
 export async function middleware(request: NextRequest) {
   try {
-    const isAuthenticated = request.cookies.get('is_auth')?.value==='true';
-    const path = request.nextUrl.pathname;
+  //   const isAuthenticated = request.cookies.get('is_auth')?.value==='true';
+  //   const path = request.nextUrl.pathname;
 
-    if (isAuthenticated) {
-      if (publicPaths.includes(path)) {
-        return NextResponse.redirect(new URL('/profile', request.url));
-      }
-    }
+  //   if (isAuthenticated) {
+  //     if (publicPaths.includes(path)) {
+  //       return NextResponse.redirect(new URL('/profile', request.url));
+  //     }
+  //   }
 
-    if (!isAuthenticated && !publicPaths.includes(path)) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-    return NextResponse.next()
-  } catch (error) {
-    console.error('Error occurred while checking authentication:', error);
-    return NextResponse.error();
-  }
+  //   if (!isAuthenticated && !publicPaths.includes(path)) {
+  //     return NextResponse.redirect(new URL('/login', request.url));
+  //   }
+  //   return NextResponse.next()
+  // } catch (error) {
+  //   console.error('Error occurred while checking authentication:', error);
+  //   return NextResponse.error();
+  // }
 }
 // This specifies which paths the middleware should run on. In this case, the middleware will apply to all paths except the ones specified in the matcher array.
 export const config = {
